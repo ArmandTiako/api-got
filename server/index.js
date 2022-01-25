90afce3901a6b73ada70bec755aef947d2cbb28e
cedricngoune <gabyngoune@yahoo.fr>
2022-01-25 15:28:59 +0100

    creating mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 const port = 5000;
+dotenv.config();
+const mongoose = require('./database/mongo-db')
 
 app.get('/', (req,res) => {
     res.send('Hello word');
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
+    mongoose.connect()
     console.log(`Listening on port ${port}`);
 })
